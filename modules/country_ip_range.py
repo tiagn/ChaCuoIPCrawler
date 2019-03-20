1bf583cafe0f4d88cbe8073d2c0ecbab3e72b5f4
tiagn <347618578@qq.com>
2019-03-20 22:19:10 +0800

    添加国家IP段模块

--- modules/country_ip_range.py
+++ modules/country_ip_range.py
@@ -1,2 +1,22 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
+from bs4 import BeautifulSoup
+
+from modules.base import BaseParser
+
+
+class Parser(BaseParser):
+
+    def _parse(self, data):
+
+        soup = BeautifulSoup(data, 'html.parser')
+        country_urls = {}
+        for ul in soup('ul', class_="list clearfix inline-block"):
+            for li in ul('li'):
+                country_urls[li.a.text] = li.a['href']
+
+        result = {
+            "clickable": country_urls,
+            "info": list(country_urls.keys())
+        }
+        return result
