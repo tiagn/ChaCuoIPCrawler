b6f4eccee5f05f6702260b28a9620ebd09f8c184
yechujiong <347618578@qq.com>
2019-03-20 17:52:48 +0800

    添加匹配模式和爬取类

--- crawler.py
+++ crawler.py
@@ -2,9 +2,12 @@
 # -*- coding:utf-8 -*-
 
 import asyncio
+import json
+
 import aiohttp
 import fake_useragent
 import logging
+import modules
 import random
 import time
 
@@ -18,13 +21,15 @@ class Downloader:
         self.loop = asyncio.get_event_loop()
 
     async def _download(self, url: str) -> Dict:
-        time.sleep(random.randint(2, 5))  # 防止访问太频繁，强制休息 2-5 秒
+        time.sleep(random.randint(1, 5) * 0.1)  # 防止访问太频繁，强制休息 0.1-0.5 秒
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=self.headers) as resp:
                 return {"url": url, "response": await resp.text(errors='ignore')}
 
     def download(self, url: str) -> Tuple[Any]:
-        return self.downloads([url])
+        resps = self.downloads([url])
+        if resps:
+            return resps[0]
 
     def downloads(self, urls: List[str]) -> Tuple[Any]:
         tasks = []
@@ -34,3 +39,46 @@ class Downloader:
             return self.loop.run_until_complete(asyncio.gather(*tasks))
         except Exception as e:
             logging.warning(f'请检查网络状态，错误：{e}')
+
+
+class Crawler:
+
+    def __init__(self):
+        self.downloader = Downloader()
+        self._data = None
+
+    def get_info(self) -> Any:
+        return self._data
+
+    def click(self, data: str) -> Any:
+        if self._data:
+            if 'clickable' in self._data:
+                if data in self._data['clickable']:
+                    self._data = self._craw(self._data['clickable'][data])
+        else:
+            if data.startswith('http'):
+                self._data = self._craw(data)
+
+    def _craw(self, urls: Any):
+        if isinstance(urls, str):
+            url = urls.strip()
+            parser = modules.find_parser(url)
+            resp = self.downloader.download(url)
+            if resp:
+                return parser(resp['response'])
+        elif isinstance(urls, list):
+            tmp_urls = []
+            for url in urls:
+                tmp_urls.append(url.strip())
+            resps = self.downloader.downloads(tmp_urls)
+            results = []
+            for resp in resps:
+                if resp:
+                    url = resp['url']
+                    parser = modules.find_parser(url)
+                    results.append(parser(resp['response']))
+
+    @staticmethod
+    def save(data: Any, filename: str) -> None:
+        with open(filename, 'wb') as fw:
+            json.dump(data, fw)
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,38 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
+import re
+from typing import Any
+
+full_url_parser = {
+    "http://ipblock.chacuo.net/": lambda x: x,
+    "http://ipcn.chacuo.net/": None,
+    "http://ips.chacuo.net/": None,
+    "http://ipblock.chacuo.net/list": None,
+    "http://as.chacuo.net/": None,
+    "http://as.chacuo.net/company": None,
+    "http://as.chacuo.net/list": None,
+}
+
+start_url_parser = {
+    "http://ipcn.chacuo.net/view/i_": None,
+    "http://ipblock.chacuo.net/view/c_": None,
+    "http://as.chacuo.net/companyview/s_": None,
+    "http://as.chacuo.net/as": None
+}
+
+regex_url_parser = {
+    r"http://as.chacuo.net/[A-Z]{2}": None
+}
+
+
+def find_parser(url: str) -> Any:
+    if url in full_url_parser.keys():
+        return full_url_parser[url]
+
+    for start_url in start_url_parser.keys():
+        if url.startswith(start_url):
+            return start_url_parser[start_url]
+
+    for regex_url in regex_url_parser.keys():
+        if re.match(regex_url, url):
+            return regex_url_parser[regex_url]
